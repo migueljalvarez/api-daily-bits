63e6889273f34683e2322cbe839c7b29ae8c0641
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2021-08-11 23:57:15 -0500

    quitando rutas personalizadas

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-// const routes = require('./routes.json')
-// const rules = auth.rewriter(routes);
-const cors = require('cors');
+const cors = require("cors");
 // /!\ Bind the router db to the app
 app.db = router.db;
 // You must apply the auth middleware before the router
 
-app.use('*', cors());
+app.use("*", cors());
 
-
-// app.use(rules)
 app.use(auth);
 app.use(router);
 const port = process.env.PORT || 4000;
